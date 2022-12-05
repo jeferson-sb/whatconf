4977cfbbcb9ac202557db24fc6cfe5053bcd03d7
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-12-05 00:24:09 -0300

    feat: add conference update procedure

--- src/server/trpc/router/conference.ts
+++ src/server/trpc/router/conference.ts
@@ -1,6 +1,19 @@
 import { publicProcedure, router } from "../trpc";
 import { z } from 'zod'
 
+export const confValidationSchema = z.object({
+  id: z.string().optional(),
+  title: z.string(),
+  description: z.string().optional(),
+  location: z.string(),
+  virtual: z.boolean(),
+  link: z.string(),
+  startDate: z.date(),
+  endDate: z.date(),
+  categoryId: z.string(),
+  organizerId: z.string()
+})
+
 export const confRouter = router({
   all: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.event.findMany();
@@ -11,23 +24,31 @@ export const confRouter = router({
       return ctx.prisma.event.findUnique({ where: { id } })
     }),
   create: publicProcedure
-    .input(z.object({
-      title: z.string(),
-      description: z.string().optional(),
-      location: z.string(),
-      virtual: z.boolean(),
-      link: z.string(),
-      startDate: z.date(),
-      endDate: z.date(),
-      categoryId: z.string(),
-      organizerId: z.string()
-    }))
-    .query(({ input, ctx }) => {
+    .input(confValidationSchema)
+    .mutation(({ input, ctx }) => {
       return ctx.prisma.event.create({
         data: {
           title: input.title,
           description: input?.description,
           location: input.location,
+          virtual: input.virtual,
+          link: input.link,
+          startDate: input.startDate,
+          endDate: input.endDate,
+          categoryId: input.categoryId,
+          organizerId: input.organizerId
+        }
+      })
+    }),
+  update: publicProcedure
+    .input(confValidationSchema)
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.event.update({
+        where: { id: input.id },
+        data: {
+          title: input.title,
+          description: input?.description,
+          location: input?.location,
           virtual: input.virtual,
           link: input.link,
           startDate: input.startDate,
