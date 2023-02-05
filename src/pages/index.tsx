f78c2160529cee4db53c12406d4d3ae6f34af609
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-02-05 15:13:36 -0300

    feat: add create button on feed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { type NextPage } from 'next'
+import Link from 'next/link'
 
 import { trpc } from '../utils/trpc'
+import { Button } from '../view/components/base/button/Button'
 import { Container } from '../view/components/container/Container'
 import { FeedList } from '../view/components/feed'
+import { Plus } from '../view/components/icons'
 
 import styles from './index.module.css'
 
@@ -12,9 +15,12 @@ const Home: NextPage = () => {
   return (
     <Container>
       <section className={styles.feed}>
+        <Button as={Link} href="/create">
+          <Plus /> <span>Submit a conf</span>
+        </Button>
         {events.isLoading ? (
           <p>Loading...</p>
-        ) : events.data ? (
+        ) : events?.data?.length ? (
           <FeedList events={events.data} />
         ) : null}
       </section>
