a50527dda9e1900473b80a68c7e91a586184d45e
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-03-05 19:38:17 -0300

    feat: add firebase messasing notification on conf subscribe

--- src/pages/agenda.tsx
+++ src/pages/agenda.tsx
@@ -1,43 +1,34 @@
 import type { NextPage } from 'next'
+
 import { trpc } from '../utils/trpc'
 import { Container } from '../view/components/container/Container'
-import { shortDateFormatted } from './formatters'
 
 import styles from './agenda.module.css'
+import { AgendaCard } from '../view/components/agenda'
 
 const Agenda: NextPage = () => {
-  const events = trpc.conference.all.useQuery()
-  const reminders = trpc.reminder.all.useQuery()
-
-  // TODO: refactor to find by id inside router instead.
-  const items = reminders?.data?.map((reminder) => {
-    const conf = events?.data?.find((event) => event.id === reminder.eventId)
-    return conf
-  })
+  const user = trpc.user.all.useQuery()
+  const currentUser = user?.data?.length ? user?.data[0] : null
+  const agendaItems = trpc.reminder.agenda.useQuery(currentUser?.id)
 
   return (
     <Container>
       <h2>Your current agenda:</h2>
       <ul className={styles.list}>
-        {items?.map((item) => (
-          <li key={item?.id}>
-            <div className={styles.card}>
-              <div className={styles.dates}>
-                <time dateTime={item?.startDate.toISOString()}>
-                  {shortDateFormatted(item?.startDate)}
-                </time>
-                <hr className={styles.divider} />
-                <time dateTime={item.startDate.toISOString()}>
-                  {shortDateFormatted(item?.endDate)}
-                </time>
-              </div>
-              <div className={styles.details}>
-                <h3>{item?.title}</h3>
-                <p>{item?.description}</p>
-              </div>
-            </div>
-          </li>
-        ))}
+        {!currentUser ? (
+          <p>You need to be logged in!</p>
+        ) : agendaItems.data?.length ? (
+          <>
+            <h3 className={styles.upcoming}>Upcoming conferences</h3>
+            {agendaItems.data.map((item) => (
+              <li key={item.id}>
+                <AgendaCard event={item.event} />
+              </li>
+            ))}
+          </>
+        ) : (
+          <p>You&apos;re not subscribed to any conference yet!</p>
+        )}
       </ul>
     </Container>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,78 @@
 import { type NextPage } from 'next'
 import Link from 'next/link'
-import { useCallback } from 'react'
-import { Conference } from '../domain/Conference'
+import { useCallback, useEffect } from 'react'
+import { getMessaging as getMessagingAdmin } from 'firebase-admin/messaging'
+import { getToken, getMessaging } from 'firebase/messaging'
+import firebase from 'firebase/compat/app'
 
 import { trpc } from '../utils/trpc'
+import { Conference } from '../domain/Conference'
 import { Button } from '../view/components/base/button/Button'
 import { Container } from '../view/components/container/Container'
 import { FeedList } from '../view/components/feed'
 import { Plus } from '../view/components/icons'
+import { isProd } from '../lib/detectEnv'
+import { vapidKey } from '../lib/firebase'
 
 import styles from './index.module.css'
 
 const Home: NextPage = () => {
   const events = trpc.conference.all.useQuery()
-  const users = trpc.user.all.useQuery()
+  const user = trpc.user.all.useQuery()
+  const reminder = trpc.reminder.create.useMutation()
 
-  const handleSubscribe = useCallback((event: Conference.Type) => {
-    // reminder.mutate({ userId: currentUser?.id, eventId: event.id })
+  const enableNotifications = useCallback(async () => {
+    try {
+      const status = await Notification.requestPermission()
+
+      if (status === 'granted' && isProd()) {
+        const messagingService = getMessaging(firebase.apps[0])
+
+        const fcm = await getToken(messagingService, { vapidKey })
+        window.localStorage.setItem('@whatconf/fcm', JSON.stringify(fcm))
+      }
+    } catch (error) {
+      console.error('An error occurred while retrieving token.', error)
+    }
+  }, [])
+
+  const handleSubscribe = useCallback(
+    async (event: Conference.Type) => {
+      try {
+        // TODO: Replace with the current authenticated user
+        const currentUser = user?.data[0]
+
+        if (!currentUser) throw new Error('No users found in this session')
+
+        const userId = currentUser?.id
+
+        reminder.mutate({ userId, eventId: event.id })
+
+        if (isProd()) {
+          const fcmToken = window.localStorage.getItem('@whatconf/fcm')
+          const response = await getMessagingAdmin().subscribeToTopic(
+            fcmToken,
+            event.title
+          )
+
+          if (response.errors) console.error(response.errors)
+        }
+
+        // TODO: Replace with an alert component
+        alert(
+          `You're now subscribed to ${event.title} and should receive notifications when the event starts!`
+        )
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new Error('Failed to subscribe user to conference', error)
+        }
+      }
+    },
+    [user?.data]
+  )
+
+  useEffect(() => {
+    enableNotifications()
   }, [])
 
   return (
