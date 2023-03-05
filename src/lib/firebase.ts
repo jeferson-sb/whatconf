b52c53cb795e7c3dec777d5ea8aa6b40b9dad6b2
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-03-05 19:34:04 -0300

    feat: setup firebase config

---
+++ src/lib/firebase.ts
@@ -0,0 +1,27 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
+import { env } from '../env/env.mjs'
+
+console.log('all env new', env)
+
+const firebaseConfig = {
+  apiKey: env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDERID,
+  appId: env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+}
+
+export const vapidKey = env.NEXT_PUBLIC_FIREBASE_VAPIDKEY
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+export const storage = firebase.storage()
