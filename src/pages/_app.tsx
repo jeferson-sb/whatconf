08cd7f5109a63f93a0181f3dfbf25be1c70056e9
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-03-15 21:55:48 -0300

    feat: next-auth authentication middleware

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
-import { type AppType } from "next/app";
+import { type AppType } from 'next/app'
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
 
-import { trpc } from "../utils/trpc";
-import "../view/styles/reset.css"
-import "../view/styles/globals.css";
+import { trpc } from '../utils/trpc'
+import '../view/styles/reset.css'
+import '../view/styles/globals.css'
 
-import Layout from "../view/components/layouts/default";
+import Layout from '../view/components/layouts/default'
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Layout><Component {...pageProps} /></Layout>;
-};
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  )
+}
 
-export default trpc.withTRPC(MyApp);
+export default trpc.withTRPC(MyApp)
--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,15 +1,38 @@
-import { initTRPC } from "@trpc/server";
-import superjson from "superjson";
+import { initTRPC, TRPCError } from '@trpc/server'
+import superjson from 'superjson'
 
-import { type Context } from "./context";
+import { type Context } from './context'
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
   errorFormatter({ shape }) {
-    return shape;
+    return shape
   },
-});
+})
 
-export const router = t.router;
-export const middleware = t.middleware;
-export const publicProcedure = t.procedure;
+/** Reusable middleware that enforces users are logged in before running the procedure. */
+const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
+  if (!ctx.session || !ctx.session.user) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+  return next({
+    ctx: {
+      // infers the `session` as non-nullable
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  })
+})
+
+export const router = t.router
+export const middleware = t.middleware
+export const publicProcedure = t.procedure
+
+/**
+ * Protected (authenticated) procedure
+ *
+ * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
+ * the session is valid and guarantees `ctx.session.user` is not null.
+ *
+ * @see https://trpc.io/docs/procedures
+ */
+export const protectedProcedure = t.procedure.use(enforceUserIsAuthed)
