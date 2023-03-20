60b0bcd25706eacb3f2fcfad1653764cd36e1631
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-03-19 23:19:59 -0300

    feat: update Button variants

--- src/view/components/base/button/Button.tsx
+++ src/view/components/base/button/Button.tsx
@@ -7,19 +7,57 @@ type ButtonProps<T extends React.ElementType> = {
   onClick?: () => void
   as?: T
   href?: string
+  colorScheme?: string
+  variant?: 'outline' | 'ghost' | 'solid'
 }
 
 const Button = <T extends React.ElementType = 'button'>(
   props: ButtonProps<T>
 ) => {
-  const { children, onClick, as = 'button', ...rest } = props
+  const {
+    children,
+    onClick,
+    as = 'button',
+    colorScheme,
+    variant,
+    ...rest
+  } = props
   const Component = as
   const classes = cx({
     [styles.button]: true,
+    [styles['button-outline']]: variant === 'outline',
   })
 
+  const colorSchemes = {
+    violet: {
+      '--_bg-color': 'var(--violet-8)',
+      '--_border-color': 'var(--violet-10)',
+      '--_bg-hover-color': 'var(--violet-9)',
+    },
+    indigo: {
+      '--_bg-color': 'var(--indigo-10)',
+      '--_border-color': 'var(--indigo-11)',
+      '--_bg-hover-color': 'var(--indigo-12)',
+    },
+    gray: {
+      '--_bg-color': 'var(--gray-4)',
+      '--_border-color': 'var(--gray-5)',
+      '--_bg-hover-color': 'var(--gray-5)',
+      '--_text-color': 'var(--gray-8)',
+    },
+  }
+
+  const dynamicStyles = {
+    ...(colorSchemes[colorScheme] ?? null),
+  }
+
   return (
-    <Component onClick={onClick} className={classes} {...rest}>
+    <Component
+      onClick={onClick}
+      className={classes}
+      style={dynamicStyles}
+      {...rest}
+    >
       {children}
     </Component>
   )
