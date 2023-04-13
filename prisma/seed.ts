2d02dd14f47caa304a4160bcde0382a0fcaa0424
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-04-12 22:17:40 -0300

    chore: update seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,3 @@
-
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -10,16 +9,36 @@ const categories = [
   'FullStack',
   'JAMstack',
   'AI',
-  'Opensource'
+  'Opensource',
 ]
 
 async function main() {
-  categories.forEach(async category => {
+  const startDate = new Date()
+  const endDate = new Date()
+  endDate.setDate(startDate.getDate() + 1)
+
+  categories.forEach(async (category) => {
     await prisma.category.upsert({
       update: { name: category },
       where: { name: category },
-      create: { name: category }
+      create: { name: category },
     })
+  })
+
+  const category = await prisma.category.findFirst()
+
+  await prisma.event.create({
+    data: {
+      title: 'Demo conf',
+      link: 'https://example.com',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non ex eget enim bibendum gravida. Fusce ut eleifend sapien. Morbi ultrices libero urna, quis cursus magna finibus non',
+      location: 'Online',
+      virtual: true,
+      startDate,
+      endDate,
+      categoryId: category?.id,
+    },
   })
 }
 
