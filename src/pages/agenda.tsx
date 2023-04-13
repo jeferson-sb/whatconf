4b8f0dc3af6ee1b6d5916a6a5e1126319f388af2
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-04-12 22:34:47 -0300

    fix: sort agenda order

--- src/pages/agenda.tsx
+++ src/pages/agenda.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
@@ -13,6 +14,14 @@ const Agenda: NextPage = () => {
   const currentUser = sessionData?.user
   const agendaItems = trpc.reminder.agenda.useQuery(currentUser?.id)
 
+  const sorted = useMemo(
+    () =>
+      agendaItems?.data?.sort((a, b) =>
+        a.event.startDate > b.event.startDate ? 1 : -1
+      ),
+    [agendaItems]
+  )
+
   return (
     <Container>
       <h2>Your current agenda:</h2>
@@ -22,10 +31,10 @@ const Agenda: NextPage = () => {
             You are not authenticated yet.{' '}
             <Link href="/api/auth/signin">Click here to log in!</Link>
           </p>
-        ) : agendaItems.data?.length ? (
+        ) : sorted?.length ? (
           <>
             <h3 className={styles.upcoming}>Upcoming conferences</h3>
-            {agendaItems.data.map((item, index) => (
+            {sorted.map((item, index) => (
               <li key={item.id} style={{ '--order': index }}>
                 <AgendaCard event={item.event} />
               </li>
