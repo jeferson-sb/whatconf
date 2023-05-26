d60bbf829d7c979df20ed09745ae31cf0ebe487c
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-05-25 21:39:02 -0300

    chore: temporary disable middleware

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -31,6 +31,7 @@ declare module 'next-auth' {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  debug: true,
   callbacks: {
     session({ session, user }) {
       if (session.user) {
