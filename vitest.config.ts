cb3c54a3dfe1685835120d6ff0f19575018e5f71
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-09-23 22:24:54 -0300

    fix: vitest alias not found

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,8 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     setupFiles: './src/__tests__/setup.ts',
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
